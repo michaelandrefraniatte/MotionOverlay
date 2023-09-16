b41320126c36120da1767548b93b9422e1a1a99b
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-09-16 04:26:32 +0200

    Add a link

---
+++ Src/XCOverlayOriginal/XCOverlay/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Web.WebView2.Core;
+using WebView2 = Microsoft.Web.WebView2.WinForms.WebView2;
+using System.Threading.Tasks;
+
+namespace XCOverlay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        public static extern uint TimeBeginPeriod(uint ms);
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        public static extern uint TimeEndPeriod(uint ms);
+        [DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
+        public static extern void NtSetTimerResolution(uint DesiredResolution, bool SetResolution, ref uint CurrentResolution);
+        public static uint CurrentResolution = 0;
+        public static bool closed = false;
+        public static int x, y, Width, Height;
+        public WebView2 webView21 = new WebView2();
+        private async void Form1_Shown(object sender, EventArgs e)
+        {
+            TimeBeginPeriod(1);
+            NtSetTimerResolution(1, true, ref CurrentResolution); 
+            Width = Screen.PrimaryScreen.Bounds.Width;
+            Height = Screen.PrimaryScreen.Bounds.Height;
+            CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--disable-web-security", "--autoplay-policy=no-user-gesture-required", "--allow-file-access-from-files");
+            CoreWebView2Environment environment = await CoreWebView2Environment.CreateAsync(null, null, options);
+            await webView21.EnsureCoreWebView2Async(environment);
+            webView21.CoreWebView2.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.All);
+            webView21.CoreWebView2.WebResourceRequested += CoreWebView2_WebResourceRequested;
+            webView21.CoreWebView2.Settings.AreDevToolsEnabled = true;
+            webView21.CoreWebView2.SetVirtualHostNameToFolderMapping("appassets", "assets", CoreWebView2HostResourceAccessKind.DenyCors);
+            webView21.Source = new Uri("https://appassets/index.html");
+            webView21.Dock = DockStyle.Fill;
+            webView21.DefaultBackgroundColor = Color.Transparent;
+            webView21.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+            this.Controls.Add(webView21);
+            this.Location = new Point(Width - this.Size.Width, Height - this.Size.Height);
+        }
+        private void CoreWebView2_WebResourceRequested(object sender, CoreWebView2WebResourceRequestedEventArgs e)
+        {
+            CoreWebView2HttpRequestHeaders requestHeaders = e.Request.Headers;
+            requestHeaders.SetHeader("Access-Control-Max-Age", "0");
+            requestHeaders.SetHeader("Cache-Control", "max-age=0, public");
+            requestHeaders.SetHeader("Access-Control-Allow-Origin", "*");
+            requestHeaders.SetHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+            requestHeaders.SetHeader("Access-Control-Allow-Headers", "x-requested-with, content-type");
+        }
+        private void CoreWebView2_NewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            e.Handled = true;
+            webView21.Source = new System.Uri(e.Uri);
+        }
+        private async Task<String> execScriptHelper(String script)
+        {
+            var x = await webView21.ExecuteScriptAsync(script).ConfigureAwait(false);
+            return x;
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            webView21.Dispose();
+        }
+    }
+}
