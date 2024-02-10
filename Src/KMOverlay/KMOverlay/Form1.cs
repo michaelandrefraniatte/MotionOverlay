1f56f3d9d90bf05f0dbf17314e7391d1dbd9380b
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-10 16:09:13 +0100

    Add about

--- Src/KMOverlay/KMOverlay/Form1.cs
+++ Src/KMOverlay/KMOverlay/Form1.cs
@@ -47,6 +47,7 @@ namespace KMOverlay
             await webView21.EnsureCoreWebView2Async(environment);
             webView21.CoreWebView2.SetVirtualHostNameToFolderMapping("appassets", "assets", CoreWebView2HostResourceAccessKind.DenyCors);
             webView21.CoreWebView2.Settings.AreDevToolsEnabled = false;
+            webView21.KeyDown += WebView21_KeyDown;
             webView21.Source = new Uri("https://appassets/index.html");
             webView21.Dock = DockStyle.Fill;
             webView21.DefaultBackgroundColor = Color.Transparent;
@@ -69,6 +70,32 @@ namespace KMOverlay
                 mouseconnected = MouseInputHookConnect();
             }
             catch { }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            OnKeyDown(e.KeyData);
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            OnKeyDown(keyData);
+            return true;
+        }
+        private void WebView21_KeyDown(object sender, KeyEventArgs e)
+        {
+            OnKeyDown(e.KeyData);
+        }
+        private void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+            }
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
